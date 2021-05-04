bd63dd76d1eb464981eecb31d1762668b98b3dbf
Bastien <bastiengiat@gmail.com>
2021-05-04 11:31:23 +0200

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by bastien giat on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
